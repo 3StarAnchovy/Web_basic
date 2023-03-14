36d4e5dbb7e776ebeaf01daf72ebdb9d64a0f5cd
홍지민 <baker0929@naver.com>
2023-03-14 16:30:20 +0900

    study

---
+++ 230314화/index.js
@@ -0,0 +1,48 @@
+console.log("실행");
+
+{
+	var a = 0;
+}
+
+console.log(a);
+
+function test1() {
+	var var1 = 10;
+}
+
+test1();
+
+//console.log(var1);
+
+var a = 30;
+console.log(window);
+
+//const var3="100";
+//var3 = "300";
+
+let arr = [1, 2, 3, 4, 5];
+
+//인덱스 뱉음
+for (let el in arr) {
+	console.log(el);
+}
+
+//요소 뱉음
+for (let el of arr) {
+	console.log(el);
+}
+
+console.log();
+let obj = {
+	"a": 1,
+	"b": 2,
+	"c": 3,
+};
+
+for (let key in obj) {
+	console.log(key);
+	console.log(obj[key]);
+}
+
+console.log(obj["a"]);
+console.log(obj.a);
